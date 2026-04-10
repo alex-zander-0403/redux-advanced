4152f5c6e88bb274958f3682d77e8403b9ac9d8b
alex-zander-0403 <alexzander0403@gmail.com>
2026-04-10 13:20:37 +0300

    add refetch fn

--- src/components/PostContainer/PostContainer.tsx
+++ src/components/PostContainer/PostContainer.tsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react";
 import { postAPI } from "../../services/PostService";
 import { PostItem } from "../PostItem/PostItem";
 
 export function PostContainer() {
-  const { data: posts, isLoading, error } = postAPI.useFetchAllPostsQuery(5);
+  const {
+    data: posts,
+    isLoading,
+    error,
+    refetch,
+  } = postAPI.useFetchAllPostsQuery(5);
 
   return (
     <div>
+      <button onClick={() => refetch()}>Refetch</button>
+
       {isLoading && <h3>Posts Loading...</h3>}
 
       {error && <h3>Ошибка загрузки постов!</h3>}
